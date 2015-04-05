e1f2546c1c5e15e58e4231ea2fd3a63eaa5e1d87
luchaoshuai <lcskey@msn.com>
2015-04-06 00:08:54 +0800

    初始化 菜单。

--- Solution/Web/App_Start/RouteConfig.cs
+++ Solution/Web/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace QinJilu.Web
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "weixin/{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
